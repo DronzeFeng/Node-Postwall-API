dc4693244a4c8fd5231ee1ebc72afc25cd1ac2a8
Joseph <yagerkiller@gmail.com>
2022-06-11 21:23:51 +0800

    Update : Users router

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,23 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const userController = require("../controllers/users");
-const handleErrorAsync = require("../services/handleErrorAsync");
-const { isAuth } = require("../services/isAuth");
+const userController = require('../controllers/users');
+const handleErrorAsync = require('../services/handleErrorAsync');
+const { isAuth } = require('../services/isAuth');
 
-router.post("/sign_up", handleErrorAsync(userController.signUp));
-router.post("/sign_in", handleErrorAsync(userController.signIn));
-router.post(
-  "/updatePassword",
+router.post('/sign_up', handleErrorAsync(userController.signUp));
+router.post('/sign_in', handleErrorAsync(userController.signIn));
+router.patch(
+  '/updatePassword',
   handleErrorAsync(isAuth),
   handleErrorAsync(userController.updatePassword)
 );
 router.get(
-  "/profile",
+  '/profile',
   handleErrorAsync(isAuth),
   handleErrorAsync(userController.getProfile)
 );
 router.patch(
-  "/profile",
+  '/profile',
   handleErrorAsync(isAuth),
   handleErrorAsync(userController.patchProfile)
 );
